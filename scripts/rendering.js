eac283b37ea8abfd07396ba199052a7e55508c5e
Guðmundur Hreiðarsson <gudmundur.hreidarsson@gmail.com>
2009-09-06 15:46:11 +0200

    utilities.js removed

--- scripts/rendering.js
+++ scripts/rendering.js
@@ -1,3 +1,10 @@
+CanvasRenderingContext2D.prototype.line = function(x0, y0, x1, y1) 
+{
+    this.beginPath();
+    this.moveTo(x0, y0);
+    this.lineTo(x1, y1);
+    this.stroke();
+};
 
 
 function ChordDrawingDecorator(chordForm, maxVisibleFrets)
--- scripts/utilities.js
+++
@@ -1,9 +0,0 @@
-CanvasRenderingContext2D.prototype.line = function(x0, y0, x1, y1) 
-{
-    this.beginPath();
-    this.moveTo(x0, y0);
-    this.lineTo(x1, y1);
-    this.stroke();
-};
-
-
